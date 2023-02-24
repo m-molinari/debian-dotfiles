bfb718ea36222c481a06bfa6c4208f04326ffb92
m-molinari <localhost@localdomain>
2023-02-24 22:24:46 +0100

    add sync script

---
+++ sync_debian_dotfiles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if  [ ! -e /etc/debian_version ] ; then
+	echo -e "\033[0;31m distro not found \033[0m"  
+	exit 1
+fi
+
+git pull
+
+if [ "$2" == "dry" ] ; then
+	rsync -avz --dry-run -b --backup-dir=old_dotfiles  --include .[^.]* --exclude .git --exclude README.md  $HOME/
+else
+	rsync -avz -b --backup-dir=old_dotfiles  --include .[^.]* --exclude .git --exclude README.md  $HOME/
+fi
+
+echo 
+echo -e  "\033[0;32m sync $DISTRO config \033[0m"
+echo
+
+exit 0
+
